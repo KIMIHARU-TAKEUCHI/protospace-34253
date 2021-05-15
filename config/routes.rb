f84ca1057dda5cd2436bdaf2d6beff858751904d
KIMIHARU-TAKEUCHI <takeuti713@gmail.com>
2021-05-15 13:28:31 +0900

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
-  root to: "messages#index"
+  root to: "prototypes#index"
+  resources :prototypes do
+    resources :comments, only: :create
+  end
+  # resources :users, only: [:edit, :update]
+  # resources :users, only: [:create, :new]
+  resources :prototypes, only: [:index, :create, :show, :new, :edit, :update, :destroy]
+  resources :users, only: :show
+
 end
